ca2efeccde5d24d3ddf279ae9edd97a73e31892e
Noppanut <Noptechnology@gmail.com>
2024-05-17 09:43:41 +0700

    Initial commit

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        children: [
+          Column(
+            children: [
+              Icon(Icons.call),
+              Text("Call"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.call),
+              Text("Call"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.call),
+              Text("Call"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
